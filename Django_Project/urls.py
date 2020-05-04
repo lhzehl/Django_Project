bf69c8f7fedaebd7b52bed19dccda024e120b896
batmanInTheJungle <german8895@gmail.com>
2020-05-04 08:47:54 +0300

    GRAPPELLI INSTALL

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
