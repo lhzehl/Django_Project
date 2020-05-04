c17c2b7855b3ad10295c8228d8d9149d26cefcd4
batmanInTheJungle <german8895@gmail.com>
2020-05-04 15:38:24 +0300

    install djoser, users permissions

--- main_object/functions.py
+++ main_object/functions.py
@@ -29,5 +29,3 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
-
-
--- main_object/views.py
+++ main_object/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework import generics
-from rest_framework.response import Response
+# from rest_framework.views import APIView
+from rest_framework import generics, permissions
+# from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 from django.db import models
 
 from .models import MainObject
 # from django.contrib.auth.models import User
 from users.models import Profile
+from .utils import MainObjectFilter
 
 
 from .serialisers import MainObjectSerializer, MainObjectDetailSerializer,\
@@ -16,54 +18,84 @@ from .serialisers import MainObjectSerializer, MainObjectDetailSerializer,\
 from .functions import get_client_ip
 
 
-class MainObjectListView(APIView):
+class MainObjectListView(generics.ListAPIView):
     """object list"""
 
-    def get(self, request):
+    # def get(self, request):
+    #     objects = MainObject.objects.filter(draft=False).annotate(
+    #         # rank_user=models.Case(
+    #         #     models.When(ranks__ip=get_client_ip(request), then=True),
+    #         #     default=False,
+    #         #     output_field=models.BooleanField()
+    #         # ),
+    #         rank_user=models.Count("ranks", filter=models.Q(ranks__ip=get_client_ip(request)))).annotate(
+    #             middle_value=models.Sum(models.F('ranks__value'))/models.Count(models.F('ranks'))
+    #         )
+    #
+    #     serializer = MainObjectSerializer(objects, many=True)
+    #     return Response(serializer.data)
+    serializer_class = MainObjectSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = MainObjectFilter
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
         objects = MainObject.objects.filter(draft=False).annotate(
-            # rank_user=models.Case(
-            #     models.When(ranks__ip=get_client_ip(request), then=True),
-            #     default=False,
-            #     output_field=models.BooleanField()
-            # ),
-            rank_user=models.Count("ranks", filter=models.Q(ranks__ip=get_client_ip(request)))).annotate(
-                middle_value=models.Sum(models.F('ranks__value'))/models.Count(models.F('ranks'))
-            )
-
-        serializer = MainObjectSerializer(objects, many=True)
-        return Response(serializer.data)
+            rank_user=models.Count(
+                "ranks", filter=models.Q(
+                    ranks__ip=get_client_ip(self.request)))).annotate(
+            middle_value=models.Sum(models.F('ranks__value')) / models.Count(models.F('ranks')))
+        return objects
 
 
-class MainObjectDetailView(APIView):
-    """object detail"""
+# class MainObjectDetailView(APIView):
+#     """object detail"""
+#
+#     def get(self, request, pk):
+#         objects = MainObject.objects.get(id=pk, draft=False)
+#         serializer = MainObjectDetailSerializer(objects)
+#         return Response(serializer.data)
 
-    def get(self, request, pk):
-        objects = MainObject.objects.get(id=pk, draft=False)
-        serializer = MainObjectDetailSerializer(objects)
-        return Response(serializer.data)
+
+class MainObjectDetailView(generics.RetrieveAPIView):
+    """ object detail view """
+    queryset = MainObject.objects.filter(draft=False)
+    serializer_class = MainObjectDetailSerializer
 
 
-class ReviewCreateView(APIView):
+# class ReviewCreateView(APIView):
+#     """add comment"""
+#
+#     def post(self, request):
+#         review = ReviewCreateSerializer(data=request.data)
+#         if review.is_valid():
+#             review.save()
+#         return Response(status=201)
+
+
+class ReviewCreateView(generics.CreateAPIView):
     """add comment"""
-
-    def post(self, request):
-        review = ReviewCreateSerializer(data=request.data)
-        if review.is_valid():
-            review.save()
-        return Response(status=201)
+    serializer_class = ReviewCreateSerializer
 
 
-class AddValueRankView(APIView):
-    """
-    add rank to object
-    """
-    def post(self, request):
-        serializer = CreateRankSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(ip=get_client_ip(request))
-            return Response(status=201)
-        else:
-            return Response(status=400)
+# class AddValueRankView(APIView):
+#     """
+#     add rank to object
+#     """
+#     def post(self, request):
+#         serializer = CreateRankSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save(ip=get_client_ip(request))
+#             return Response(status=201)
+#         else:
+#             return Response(status=400)
+
+
+class AddValueRankView(generics.CreateAPIView):
+    serializer_class = CreateRankSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(ip=get_client_ip(self.request))
 
 
 class ProfileListView(generics.ListAPIView):
